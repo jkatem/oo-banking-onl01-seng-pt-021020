24ac30d3d563197191901464615954de8ca75ca7
Jen Miao <jenmiao@gmail.com>
2020-03-11 02:22:03 +0000

    Automatically backed up by Learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,15 @@
 class BankAccount
 
+  attr_reader :name
+
+  def initialize(name, balance = 1000)
+    @name = name
+    @balance = balance
+    @status = 'open'
+  end
+
+  def deposit(money)
+    @balance += money 
+  end
+
 end
