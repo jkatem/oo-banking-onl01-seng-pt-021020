dfecc02d1c2a8afb1f23009c035211e76dad4174
Jen Miao <jenmiao@gmail.com>
2020-03-11 03:03:32 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -12,9 +12,25 @@ class Transfer
   end
 
   def valid?
-    @sender.valid? && @receiver.valid? 
+    @sender.valid? && @receiver.valid?
   end
 
+  def execute_transaction
+    if valid? && @sender.balance > @amount && @status == "pending"
+      @sender.deposit(@amount * -1)
+      @receiver.deposit(@amount)
+      @status = "complete"
+    else
+      @status = "rejected"
+      return "Transaction rejected. Please check your account balance."
+    end
+  end
 
-
+  def reverse_transfer
+    if @status == "complete"
+      @sender.balance = @sender.balance + @amount
+      @receiver.balance = @receiver.balance - @amount
+      @status = "reversed"
+    end
+  end
 end
